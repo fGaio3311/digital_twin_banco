4d4295e3b5fc4ec1dfa51dcc8cbfab2c1eabc4c0
Davi Mateus Gaio <davimateusgaio@MacBook-Air-de-Andre-2.local>
2026-05-06 20:44:24 -0300

    chore: remove legacy frontend directories

--- frontend/src/dashboard/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Dashboard from './Dashboard';
-import './Dashboard.css';
-
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
